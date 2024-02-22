4b08e140f7f3f51f708b9d417f39305a5f5c8dbe
unknown <arjithpillai72486@gmail.com>
2024-02-22 14:45:23 +0530

    Javascript project 1

---
+++ JavaScript scroll effect/script.js
@@ -0,0 +1,10 @@
+let indicator = document.querySelector(".scroll-indicator .progress");
+let scrollHeight =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+console.log(document.documentElement);
+window.addEventListener("scroll", scroll);
+function scroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrolled = Math.trunc((scrollTop / scrollHeight) * 100);
+  indicator.style.width = `${scrolled}%`;
+}
